c786f2c6a6e929ea1dc550babe09dbc0adbdd033
Esteban Silva Pastrana <144711261+estebansilvap@users.noreply.github.com>
2026-08-29 16:37:48 -0500

    Delete backend_node1 directory

--- backend_node1/Pattern/singleton.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Bdds16-'
-    MYSQL_DB = 'orders_db'
-    MYSQL_CURSORCLASS = 'DictCursor'
-    SECRET_KEY = 'tu_clave_secreta'
